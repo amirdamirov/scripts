c83f13db4dc8a2d5b2e59b85dd8fae9e806a65d0
amirdamirov <36473660+amirdamirov@users.noreply.github.com>
2019-12-24 14:44:14 +0300

    Create sed.sh

---
+++ bash/sed.sh
@@ -0,0 +1,10 @@
+### If you have tsv file, this script will add HTML tags to this file for generating to as an page.
+### Example:   sed.sh test.tsv
+
+
+#!/bin/sed -f 
+s/^/<TR><TD>/
+s/$/<\/TD><\/TR>/
+s/\t/<\/TD><TD>/g
+1s/^/<TABLE>/
+$a<\/TABLE>
